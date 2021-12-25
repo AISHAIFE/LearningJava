4ae3a940cb7f50dcb7f6ede2d3350518b22bc07f
AISHAIFE <aishaabdulwahab181@gmail.com>
2021-12-26 00:03:47 +0100

    I just learny how to declare variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,10 @@
 
 public class HelloWorld {
 	
+/* The below is the main method */
 	public static void main (String[]args) {
 		
+		//The below will print Hello world 
 		System.out.println("Hello World");
 	}
 
